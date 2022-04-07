2ccf9b169a03482ec4865208d301d73b79180a19
akitelesforo <akitelesforo@yahoo.com>
2022-04-07 16:48:34 +0800

    Added NBomber

--- IdiotBenchmark.API.PerformanceTest/Program.cs
+++ IdiotBenchmark.API.PerformanceTest/Program.cs
@@ -1,12 +1,16 @@
-﻿const string strCmdText = "/C k6 run .\\smoke-test.js";
+﻿using IdiotBenchmark.API.PerformanceTest;
+
+const string strCmdText = "/C k6 run .\\smoke-test.js";
 // const string strCmdText = "/C k6 run .\\load-test.js";
 // const string strCmdText = "/C k6 run .\\soak-test.js";
 // const string strCmdText = "/C k6 run .\\stress-test.js";
 
-System.Diagnostics.Process process = new();
-System.Diagnostics.ProcessStartInfo startInfo = new();
-startInfo.FileName = "cmd.exe";
-startInfo.Arguments = strCmdText;
-process.StartInfo = startInfo;
-process.Start();
-process.WaitForExit();
+// System.Diagnostics.Process process = new();
+// System.Diagnostics.ProcessStartInfo startInfo = new();
+// startInfo.FileName = "cmd.exe";
+// startInfo.Arguments = strCmdText;
+// process.StartInfo = startInfo;
+// process.Start();
+// process.WaitForExit();
+
+HttpTest.Run();
